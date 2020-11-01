61a138ee4014622170f42e1c85fb0ef3c98e4051
Justin Hernandez <justin@circavictor.com>
2020-10-31 17:31:16 -0700

    add empty problem

---
+++ epi_judge_javascript/count_bits.js
@@ -0,0 +1,11 @@
+const test = require('./test_framework');
+
+
+// write solution here
+function count_bits(x) {
+  // TODO - you fill in here.
+  return 0;
+}
+
+
+test('generic', 'count_bits', count_bits);
