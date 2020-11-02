5d121cb09c6a6e80965de62f200e2a404a7fa7d3
Justin Hernandez <justin@circavictor.com>
2020-11-01 21:51:33 -0800

    prettier

--- epi_judge_javascript_solutions/test_framework/tools/convert.js
+++ epi_judge_javascript_solutions/test_framework/tools/convert.js
@@ -1,7 +1,6 @@
-
 // this little helper will extend the problem_mapping.js variable
 // and add javascript while keeping original formatting
-const mappings = require("./data/problem_mapping.old");
+const mappings = require('./data/problem_mapping.old');
 function covertProblemMappings() {
   let m = Object.assign({}, mappings);
 
@@ -9,7 +8,7 @@ function covertProblemMappings() {
   Object.keys(m).forEach((chapter) => {
     // iterate through problems
     Object.keys(m[chapter]).forEach((problem) => {
-      let pythonLang = "";
+      let pythonLang = '';
       // find python key
       Object.keys(m[chapter][problem]).forEach((language) => {
         if (language.indexOf('Python') === 0) {
@@ -18,7 +17,9 @@ function covertProblemMappings() {
       });
 
       // let's update the python key and turn it into a javascript key
-      const jsLang = pythonLang.replace('Python', 'Javascript').replace('.py', '.js');
+      const jsLang = pythonLang
+        .replace('Python', 'Javascript')
+        .replace('.py', '.js');
 
       // add javascript option to list of problems
       m[chapter][problem][jsLang] = m[chapter][problem][pythonLang];
